23c5dbeb39fa3b247cfd22a014896669ee13776f
MikaelBenhaiem <mik.benhaiem@gmail.com>
2015-04-06 11:43:22 +0000

    Incomplet

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ var bodyP = require('body-parser');
 var cookieP = require('cookie-parser');
 
 var app = express();
-var mysql = require('mysql');
+
 var twig = require('twig');
+var mysql = require('mysql');
 var db = mysql.createConnection({
     host    : process.env.IP,
     user    : process.env.C9_USER.substr(0,16),
@@ -15,13 +16,21 @@ var db = mysql.createConnection({
 
 var session = require('express-session');
 
-var connectes = [];
-
 app
     .use(bodyP.json())
     .use(bodyP.urlencoded({ extended: false }))
     .use(cookieP())
     .use(session({ secret: '12345' }));
+    
 app.set('views', '');
 app.set('view engine', 'html');
+//app.set('views', 'Calendrier');
 app.engine('html', twig.__express);
+
+app.all('/signup', function(req,res)
+{
+    
+    res.render('signup.twig');
+});
+
+app.listen(8080);
