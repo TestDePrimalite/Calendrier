baf6ac8b4aa66f57dbdb0cde84c482412e24e29e
MikaelBenhaiem <mik.benhaiem@gmail.com>
2015-04-07 13:05:30 +0000

    Ajout gestionnaire connexion/deco

--- app.js
+++ app.js
@@ -24,13 +24,128 @@ app
     
 app.set('views', '');
 app.set('view engine', 'html');
-//app.set('views', 'Calendrier');
 app.engine('html', twig.__express);
 
 app.all('/signup', function(req,res)
 {
-    
-    res.render('signup.twig');
+    if(req.session.login != undefined)
+    {
+        console.log(req.session.login);
+        res.redirect('/');
+    }
+    else
+    {
+        if(req.method == "POST")
+        {
+            console.log("Login : " + req.body.login);
+            console.log("Pass : " + req.body.pass);
+            if(req.body.login.trim() != "" && req.body.pass.trim() != "")
+            {
+                db.query('INSERT INTO utilisateurs VALUES (?,?)', [req.body.login,req.body.pass],
+                function(err,result)
+                {
+                    if(err)
+                    {
+                        console.log(err);
+                        //if(err.code == "ER_DUP_ENTRY")
+                        res.render('signup.twig', {'erreur' : 1});
+                    }
+                    else if(result.length != 0)
+                    {
+                        console.log(result);
+                        req.session.login = req.body.login;
+                        req.session.pass = req.body.pass;
+                        console.log(req.session.pseudo);
+                        console.log(req.session.pass);
+                        res.redirect('/');
+                    }
+                    else
+                    {
+                        console.log("Resultat vide...");
+                        res.render('signup.twig');
+                    }
+                });
+            }
+            else if(req.body.login.trim() == "")
+            {
+                res.render('signup.twig', {'erreur' : 2});
+            }
+            else if(req.body.pass.trim() == "")
+            {
+                res.render('signup.twig', {'erreur' : 3});
+            }
+        }
+        else
+        {
+            res.render('signup.twig');
+        }
+    }
+});
+
+app.get('/', function(req,res)
+{
+   res.render('calendrier.twig', {'login' : req.session.login});
+});
+
+app.all('/login', function(req,res)
+{
+    if(req.session.login != undefined)
+    {
+        console.log(req.session.login);
+        res.redirect('/');
+    }
+    else
+    {
+        console.log(req.method);
+        if(req.method == "POST")
+        {
+            console.log("Login : " + req.body.login);
+            console.log("Pass : " + req.body.pass);
+            if(req.body.login.trim() != "" && req.body.pass.trim() != "")
+            {
+                db.query("SELECT * FROM utilisateurs WHERE login=(?) AND pass=(?)", [req.body.login,req.body.pass],
+                function(err,result)
+                {
+                    if(err)
+                    {
+                        console.log(err);
+                        res.render('login.twig', {'erreur' : 1});
+                    }
+                    else if(result.length != 0)
+                    {
+                        console.log(result);
+                        req.session.login = req.body.login;
+                        req.session.pass = req.body.pass;
+                        res.redirect('/');
+                    }
+                    else
+                    {
+                        console.log("Resultat vide");
+                        res.render('login.twig', {'erreur' : 2});
+                    }
+                });
+            }
+            else if(req.body.login.trim() == "")
+            {
+                res.render('login.twig', {'erreur' : 3});
+            }
+            else if(req.body.pass.trim() == "")
+            {
+                res.render('login.twig', {'erreur' : 4});
+            }
+        }
+        else
+        {
+            res.render('login.twig');
+        }
+    }
+});
+
+app.get('/logout', function(req,res)
+{
+    req.session.login = null;
+    req.session.pass = null;
+    res.redirect('/');
 });
 
 app.listen(8080);
